200cf042024bc241a28d2bac16a842fac680bbdb
Bradley Fourie <30635808+bradfourie@users.noreply.github.com>
2018-03-07 12:44:52 +0200

    Update Shooter.java

--- Shooter.java
+++ Shooter.java
@@ -9,4 +9,28 @@ public class Shooter extends DefaultCritter{
 	public Shooter(){
 		
 	}
+	
+	public void setCoordsTurret(double xCoordTurret, double yCoordTurret){
+		this.xCoordTurret = xCoordTurret;
+		this.yCoordTurret = yCoordTurret;
+	}
+	public void setRadialVelocityTurret(double radialVelocity){
+		this.radialVelocity = radialVelocity;	
+	}
+	public void setAngleTurret(){
+		this.angleTurret = angleTurret;
+	}
+	
+	public double[] getCoordsTurret(){
+		double[] currentCoordsTurret = {xCoordTurret, yCoordTurret};
+		return currentCoordsTurret;
+	}
+	public double getRadialVelocityTurret(){
+		return radialVelocityTurret;
+	}
+	public double getAngleTurret(){
+		return angleTurret;
+	}
+	
+	
 }
