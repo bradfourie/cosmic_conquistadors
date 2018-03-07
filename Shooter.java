a6c70d54c280319a2d852434800323b65e74b5f0
Bradley Fourie <30635808+bradfourie@users.noreply.github.com>
2018-03-07 12:05:47 +0200

    Update Shooter.java

--- Shooter.java
+++ Shooter.java
@@ -1,3 +1,12 @@
 public class Shooter{
 	
+	private Boolean isShoot;
+	private double xCoordTurret;
+	private double yCoordShooter;
+	private double radialVelocityTurret;
+	private double angleTurret;
+	
+	public Shooter(){
+		
+	}
 }
