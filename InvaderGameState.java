81310e9262c85721a261ee09f1cf34daf562a28f
Heinrich12345 <37140717+Heinrich12345@users.noreply.github.com>
2018-03-11 12:57:48 +0200

    Updated Draw and added Shooter object

--- InvaderGameState.java
+++ InvaderGameState.java
@@ -1,16 +1,18 @@
 public class InvaderGameState{
 	ArrayList<Enemy> enemies = new ArrayList<Enemy>(0);
 	ArrayList<Missile> missiles = new ArrayList<Missile>(0);
+	Shooter shooter;
 	//handle the state of the game, i.e. menu or in game
 	//handle key presses
 	//update draw
 	Public void Draw(){
+		StdDraw.clear();
 		for(int i=0;i<enemies.length;i++){
-			enemies.get(i);
+			enemies.get(i).move;
 		}
 		for(int i=0;i<missiles.length;i++){
-			enemies.get(i);
+			enemies.get(i).move;
 		}
-		
+		shooter.move;
 	}
 }
