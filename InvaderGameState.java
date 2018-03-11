4442beaeb446adb52085fae4a5e5379e9ed0da6d
Heinrich12345 <37140717+Heinrich12345@users.noreply.github.com>
2018-03-11 12:49:45 +0200

    Added Missile and Enemy lists and Draw function

--- InvaderGameState.java
+++ InvaderGameState.java
@@ -1,5 +1,16 @@
 public class InvaderGameState{
+	ArrayList<Enemy> enemies = new ArrayList<Enemy>(0);
+	ArrayList<Missile> missiles = new ArrayList<Missile>(0);
 	//handle the state of the game, i.e. menu or in game
 	//handle key presses
 	//update draw
+	Public void Draw(){
+		for(int i=0;i<enemies.length;i++){
+			enemies.get(i);
+		}
+		for(int i=0;i<missiles.length;i++){
+			enemies.get(i);
+		}
+		
+	}
 }
