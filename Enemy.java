bd50eb482d101e8f98c03757413476e8e2292842
danielbanks98 <36916490+danielbanks98@users.noreply.github.com>
2018-03-13 10:09:34 +0200

    Updated move method

--- Enemy.java
+++ Enemy.java
@@ -9,11 +9,23 @@ public class Enemy extends DefaultCritter {
 	private void onMissileCollision(){
 		
 	}
-	
-	public void move(){
-		super.xCoord = super.xCoord + super.xVelocity;
-		super.yCoord = super.yCoord + super.yVelocity;
-		StdDraw.setPenColor(StdDraw.RED);
-        	StdDraw.filledCircle(super.xCoord,super.yCoord,radius);
-	}
+ 
+ 	public void move(){
+   		double xVelocityTemp = super.xVelocity; // using this so i can set the xVelocity of the enemy to 0 just for one time step
+   		boolean isBounce = false;
+   		if(super.xCoord == 0 || super.xCoord == 100){
+     			isBounce = true;
+     			super.xVelocity = 0;
+     			super.yVelocity = -0.04;
+   		}
+  		super.xCoord = super.xCoord + super.xVelocity;
+  		super.yCoord = super.yCoord + super.yVelocity;
+  		StdDraw.setPenColor(StdDraw.RED);
+         	StdDraw.filledCircle(super.xCoord,super.yCoord,radius);
+         	if(isBounce){
+            		super.xVelocity = -xVelocityTemp; //setting xVelocity to opposite of what it was before
+            		super.yVelocity = 0;
+         	}
+        
+ 	}
 }
