793dc6efcb848824094c134e8397f36952120e36
Bradley Fourie <30635808+bradfourie@users.noreply.github.com>
2018-03-16 22:09:29 +0200

    Fixed constructor

--- Missile.java
+++ Missile.java
@@ -3,23 +3,15 @@ public class Missile extends DefaultCritter{
 	private int numBounced; //counts the number of bounces a missile has made
 	
 	public Missile(Shooter shooter){
-		double velocity = 30;
-		double radius = 10;
-		
+		super(shooter.getXCoordBarrel(), shooter.getYCoordBarrel(), 30 * Math.cos(initialAngle), 30 * Math.sin(initialAngle), 10);
 		this.numBounced = 0;
 		
 		//The initial x and y coordinates of the missile
 		//is the same as that of the x and y coordinates
-		//of the shooter at the instant the missile is created
+		//of the shooter's barrel at the instant the missile
+		//is created
+		//ill add proper comments here tomorrow
 		
-		double xCoordBarrel = shooter.getXCoordBarrel();
-		double yCoordBarrel = shooter.getYCoordBarrel();
-		double initialAngle = shooter.getAngleBarrel();
-		
-		double xVelocity = velocity * Math.cos(initialAngle);
-		double yVelocity = velocity * Math.sin(initialAngle);
-		
-		super(xCoordbarrel, yCoordBarrel, xVelocityBarrel, yVelocityBarrel, radius);
 	}
 	
 	public int getNumBounced(){
