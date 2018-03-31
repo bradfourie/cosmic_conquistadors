718da179c8a97c65bb93b7c66ae031823e60a6a1
Bradley Fourie <30635808+bradfourie@users.noreply.github.com>
2018-03-31 20:45:36 +0200

    Fixed indentation

--- Enemy.java
+++ Enemy.java
@@ -1,44 +1,45 @@
 public class Enemy extends DefaultCritter {
-  
-  public Enemy(double xCoord, double yCoord, double xVelocity, double yVelocity){
-    super(xCoord, yCoord, xVelocity, yVelocity,3); // final param radius
-  }
-  
-  public boolean onMissileCollision(Missile missile){
-    double radiusMissile = missile.getRadius();
-    double distance = Math.sqrt( Math.pow(missile.getXCoord() - super.xCoord, 2) + Math.pow(missile.getYCoord() - super.yCoord, 2));
-    
-    if(distance <= (radiusMissile + super.radius)){
-      return true;
-    }else{
-      return false;    
+
+    public Enemy(double xCoord, double yCoord, double xVelocity, double yVelocity){
+        super(xCoord, yCoord, xVelocity, yVelocity, 3); // final param radius
     }
-    
-  }
-  
-  public void move(){
-    double xVelocityTemp = super.xVelocity; // using this so i can set the xVelocity of the enemy to 0 just for one time step
-    boolean isBounce = false;
-    if((super.xCoord - super.radius) <= -100 || (super.xCoord + super.radius) >= 100){
-      isBounce = true;
-      super.xVelocity = 0;
-      super.yVelocity = -(super.radius*2);
+
+    public boolean onMissileCollision(Missile missile){
+
+        double radiusMissile = missile.getRadius();
+        double distance = Math.sqrt( Math.pow(missile.getXCoord() - super.xCoord, 2) + Math.pow(missile.getYCoord() - super.yCoord, 2));
+
+        if(distance <= (radiusMissile + super.radius)){
+            return true;
+        }else{
+            return false;
+        }
+
     }
-    super.xCoord = super.xCoord + super.xVelocity;
-    super.yCoord = super.yCoord + super.yVelocity;
-    StdDraw.setPenColor(StdDraw.RED);
-    StdDraw.filledCircle(super.xCoord,super.yCoord,super.radius);
-    if(isBounce){
-      super.xVelocity = -xVelocityTemp; //setting xVelocity to opposite of what it was before
-      super.yVelocity = 0;
-      super.xCoord = super.xCoord + super.xVelocity;
-      super.yCoord = super.yCoord + super.yVelocity;
-      StdDraw.setPenColor(StdDraw.RED);
-      StdDraw.filledCircle(super.xCoord,super.yCoord,super.radius);
+
+    public void move(){
+        double xVelocityTemp = super.xVelocity; // using this so i can set the xVelocity of the enemy to 0 just for one time step
+        boolean isBounce = false;
+        if((super.xCoord - super.radius) <= -100 || (super.xCoord + super.radius) >= 100){
+            isBounce = true;
+            super.xVelocity = 0;
+            super.yVelocity = -(super.radius*2);
+        }
+        super.xCoord = super.xCoord + super.xVelocity;
+        super.yCoord = super.yCoord + super.yVelocity;
+        render();
+
+        if(isBounce){
+            super.xVelocity = -xVelocityTemp; //setting xVelocity to opposite of what it was before
+            super.yVelocity = 0;
+            super.xCoord = super.xCoord + super.xVelocity;
+            super.yCoord = super.yCoord + super.yVelocity;
+            render();
+        }
     }
-    
+
     public void render(){
-      // add code to render here instead of in move function
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.filledCircle(super.xCoord,super.yCoord,super.radius);
     }
-  }
 }
