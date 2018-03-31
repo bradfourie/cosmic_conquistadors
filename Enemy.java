41f74501eca2a1bc43b6cd22bfa7b0728cf9c457
danielbanks98 <36916490+danielbanks98@users.noreply.github.com>
2018-03-31 08:55:07 +0200

    Update Enemy.java
    Movement and deletion works (Movement sort of works), if you look at the movement they dont stay aligned sometimes.

--- Enemy.java
+++ Enemy.java
@@ -1,31 +1,43 @@
 public class Enemy extends DefaultCritter {
-	
-	public Enemy(double xCoord, double yCoord, double xVelocity, double yVelocity){
-		double radius = 0.02;
-		super(xCoord, yCoord, xVelocity, yVelocity);
-		
-	}
-	
-	private void onMissileCollision(){
-		
-	}
- 
- 	public void move(){
-   		double xVelocityTemp = super.xVelocity; // using this so i can set the xVelocity of the enemy to 0 just for one time step
-   		boolean isBounce = false;
-   		if(super.xCoord == 0 || super.xCoord == 100){
-     			isBounce = true;
-     			super.xVelocity = 0;
-     			super.yVelocity = -0.04;
-   		}
-  		super.xCoord = super.xCoord + super.xVelocity;
-  		super.yCoord = super.yCoord + super.yVelocity;
-  		StdDraw.setPenColor(StdDraw.RED);
-         	StdDraw.filledCircle(super.xCoord,super.yCoord,radius);
-         	if(isBounce){
-            		super.xVelocity = -xVelocityTemp; //setting xVelocity to opposite of what it was before
-            		super.yVelocity = 0;
-         	}
-        
- 	}
+  
+  public Enemy(double xCoord, double yCoord, double xVelocity, double yVelocity){
+    super(xCoord, yCoord, xVelocity, yVelocity,3); // final param radius
+    
+  }
+  
+  public boolean onMissileCollision(Missile missile){
+    double rMissile = missile.getRadius();
+    double distance = Math.sqrt( Math.pow(missile.getXCoord() - super.xCoord, 2) + Math.pow(missile.getYCoord() - super.yCoord, 2));
+    if(distance <= (rMissile + super.radius)){
+     
+      return true;
+    }else{
+      
+      return false; 
+      
+    }
+  }
+  
+  public void move(){
+    double xVelocityTemp = super.xVelocity; // using this so i can set the xVelocity of the enemy to 0 just for one time step
+    boolean isBounce = false;
+    if((super.xCoord - super.radius) <= -100 || (super.xCoord + super.radius) >= 100){
+      isBounce = true;
+      super.xVelocity = 0;
+      super.yVelocity = -(super.radius*2);
+    }
+    super.xCoord = super.xCoord + super.xVelocity;
+    super.yCoord = super.yCoord + super.yVelocity;
+    StdDraw.setPenColor(StdDraw.RED);
+    StdDraw.filledCircle(super.xCoord,super.yCoord,super.radius);
+    if(isBounce){
+      super.xVelocity = -xVelocityTemp; //setting xVelocity to opposite of what it was before
+      super.yVelocity = 0;
+      super.xCoord = super.xCoord + super.xVelocity;
+      super.yCoord = super.yCoord + super.yVelocity;
+      StdDraw.setPenColor(StdDraw.RED);
+      StdDraw.filledCircle(super.xCoord,super.yCoord,super.radius);
+    }
+    
+  }
 }
