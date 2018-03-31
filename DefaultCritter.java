406ad8280d8e9cd7fac3adc3ec929f5709d3b6d0
Bradley Fourie <30635808+bradfourie@users.noreply.github.com>
2018-03-31 20:44:36 +0200

    Removed unnecessary comments

--- DefaultCritter.java
+++ DefaultCritter.java
@@ -1,45 +1,41 @@
 public class DefaultCritter{
-	
-	protected double xCoord;
-	protected double yCoord;
-	protected double xVelocity;
-	protected double yVelocity;
-	protected double radius;
-	
-	public DefaultCritter(double xCoord, double yCoord, double xVelocity, double yVelocity, double radius){
-		this.xCoord = xCoord;
-		this.yCoord = yCoord;
-		this.xVelocity = xVelocity;
-		this.yVelocity = yVelocity;
-		this.radius = radius;
-	}
-	
-	//setters
-	public void setXCoord(double xCoord){
-		this.xCoord = xCoord;
-	}
-	public void setYCoord(double yCoord){
-		this.yCoord = yCoord;
-	}
-	public void setXVelocity(double xVelocity){
-		this.xVelocity = xVelocity;
-	}
-	public void setYVelocity(double yVelocity){
-		this.yVelocity = yVelocity;
-	}
-	//this method is reduntant
-	/*protected double setRadius(double radius){
-		this.radius = radius;
-	}*/
-	
-	//getters
-	public double getYCoord(){
-		return yCoord;
-	}
-	public double getXCoord(){
-		return xCoord;
-	}
-	public double getRadius(){
-		return radius;
-	}
+
+    protected double xCoord;
+    protected double yCoord;
+    protected double xVelocity;
+    protected double yVelocity;
+    protected double radius;
+
+    public DefaultCritter(double xCoord, double yCoord, double xVelocity, double yVelocity, double radius){
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
+        this.xVelocity = xVelocity;
+        this.yVelocity = yVelocity;
+        this.radius = radius;
+    }
+
+    //setters
+    public void setXCoord(double xCoord){
+        this.xCoord = xCoord;
+    }
+    public void setYCoord(double yCoord){
+        this.yCoord = yCoord;
+    }
+    public void setXVelocity(double xVelocity){
+        this.xVelocity = xVelocity;
+    }
+    public void setYVelocity(double yVelocity){
+        this.yVelocity = yVelocity;
+    }
+
+    //getters
+    public double getYCoord(){
+        return yCoord;
+    }
+    public double getXCoord(){
+        return xCoord;
+    }
+    public double getRadius(){
+        return radius;
+    }
 }
