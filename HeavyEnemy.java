74b69156ebd3732d81e53f16f85cd7ea79bf62df
Bradley Fourie <30635808+bradfourie@users.noreply.github.com>
2018-04-21 18:56:19 +0200

    Update HeavyEnemy.java

--- HeavyEnemy.java
+++ HeavyEnemy.java
@@ -4,12 +4,6 @@ public class HeavyEnemy extends Enemy {
     }
 
     public void render(){
-        //just for now
-        if(getLives() >= 2){
-          StdDraw.picture(super.xCoord,super.yCoord,"EnemyRed.png",super.radius*2,super.radius*2);
-        }
-        if(getLives() == 1){
-          StdDraw.picture(super.xCoord,super.yCoord,"EnemyYellow.png",super.radius*2,super.radius*2);
-        }
+        StdDraw.picture(super.xCoord,super.yCoord,"EnemyYellow.png",super.radius*2,super.radius*2);
     }
 }
