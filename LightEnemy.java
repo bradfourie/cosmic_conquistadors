e380c55b4d29aa667a6f11f3cfed8f7cf5201bfb
Bradley Fourie <30635808+bradfourie@users.noreply.github.com>
2018-04-21 18:59:48 +0200

    Update LightEnemy.java

--- LightEnemy.java
+++ LightEnemy.java
@@ -1,16 +1,15 @@
+
 public class LightEnemy extends Enemy {
     public LightEnemy(double xCoord, double yCoord, double xVelocity, double yVelocity){
-        super(xCoord, yCoord, xVelocity, yVelocity,10 , 2);
+        super(xCoord, yCoord, xVelocity, yVelocity,15 , 2);
     }
 
     public void render(){
-        if(getLives() == 2){
-            StdDraw.picture(super.xCoord,super.yCoord,"EnemyRed.png",super.radius*2,super.radius*2);
-        }
-        if(getLives() == 1){
-            StdDraw.picture(super.xCoord,super.yCoord,"EnemyYellow.png",super.radius*2,super.radius*2);
-        }
+
+        StdDraw.picture(super.xCoord,super.yCoord,"EnemyRed.png",super.radius*2,super.radius*2);
+
         
     }
 
 }
+
