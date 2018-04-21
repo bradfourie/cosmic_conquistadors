e44bf2fb44bde8e65eb2c0a253bc704c82e163be
Bradley Fourie <30635808+bradfourie@users.noreply.github.com>
2018-04-21 19:00:48 +0200

    Update NormalMissile.java

--- NormalMissile.java
+++ NormalMissile.java
@@ -1,19 +1,15 @@
+
 public class NormalMissile extends Missile{
-  
+
   public NormalMissile(Shooter shooter){
-    super(shooter.getXCoord(), shooter.getYCoord(), 15 * Math.cos(shooter.getAngleBarrel()*Math.PI/180+Math.PI/2), 15 * Math.sin(shooter.getAngleBarrel()*Math.PI/180+Math.PI/2), 3);
-    super.xVelocity = 15*Math.cos(shooter.getAngleBarrel()*Math.PI/180+Math.PI/2);
-    super.yVelocity = 15*Math.sin(shooter.getAngleBarrel()*Math.PI/180+Math.PI/2);
-    super.angle = shooter.getAngleBarrel()+90;
+    super(shooter.getXCoordBarrel(), shooter.getYCoordBarrel(), 15 * Math.cos(shooter.getAngleBarrel()*Math.PI/180), 15 * Math.sin(shooter.getAngleBarrel()*Math.PI/180), 3);
   }
-  
-  public void moveAngle(double a){
-    super.angle = super.angle + a;
-    super.xVelocity = 15*Math.cos(super.angle * (Math.PI/180));
-    super.yVelocity = 15*Math.sin(super.angle * (Math.PI/180));
+
+  public NormalMissile(Shooter shooter, double angleOffset){
+    super(shooter.getXCoordBarrel(), shooter.getYCoordBarrel(), 15 * Math.cos((shooter.getAngleBarrel() + angleOffset)*Math.PI/180), 15 * Math.sin((shooter.getAngleBarrel() + angleOffset)*Math.PI/180), 3);
   }
   
   public void render(){
-    StdDraw.picture(super.xCoord,super.yCoord,"BulletYellow.png",super.radius*5,super.radius*2,angle);
+    StdDraw.picture(super.xCoord,super.yCoord,"BulletYellow.png",super.radius*5,super.radius*2, super.angle);
   }
 }
