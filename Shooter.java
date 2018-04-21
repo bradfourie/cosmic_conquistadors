73ff9fa843f55ce94b1012e5288a012d1d49d5a9
Bradley Fourie <30635808+bradfourie@users.noreply.github.com>
2018-04-21 19:01:30 +0200

    Update Shooter.java

--- Shooter.java
+++ Shooter.java
@@ -1,38 +1,28 @@
+
 public class Shooter extends DefaultCritter{
 
-    private double INITIAL_ANGLE_BARREL = Math.PI/2.0;
     private double xCoordBarrel;
     private double yCoordBarrel;
     private double radialVelocityBarrel;
-    private double radiusBarrel;
     private double angleBarrel;
     private int lives;
     private int powerUp;
 
     public Shooter(double xCoord, double yCoord, double xVelocity, double yVelocity, int lives){
         super(xCoord, yCoord, xVelocity, yVelocity, 10);
-        angleBarrel = 0;
-        radiusBarrel = super.radius/2.0;
-        xCoordBarrel = super.radius*Math.cos(radiusBarrel);
-        yCoordBarrel = super.radius*Math.sin(radiusBarrel);
+        angleBarrel = 90;
+        xCoordBarrel = super.xCoord;
+        yCoordBarrel = super.getRadius() + super.yCoord;
         this.lives = lives;
     }
 
     /* Setters */
-    public void setRadialVelocityBarrel(double radialVelocityBarrel){
-        this.radialVelocityBarrel = radialVelocityBarrel;
-    }
+    public void setRadialVelocityBarrel(double radialVelocityBarrel){ this.radialVelocityBarrel = radialVelocityBarrel; }
     public void setPowerUp(int power){
       powerUp = power;
     }
 
     /* Getters */
-    public double getRadialVelocityBarrel(){
-        return radialVelocityBarrel;
-    }
-    public double getRadiusBarrel(){
-        return radiusBarrel;
-    }
     public double getAngleBarrel(){
         return angleBarrel;
     }
@@ -42,68 +32,48 @@ public class Shooter extends DefaultCritter{
     public double getYCoordBarrel(){
         return yCoordBarrel;
     }
+
     public int getLives(){
         return lives;
     }
-
     public double getPower(){
     return powerUp;  
-    }
-    
-    public double getXCoord(){
-     return super.xCoord; 
-    }
-    
-    public double getYCoord(){
-      return super.yCoord;
-    }
-    
-    public double getRadius(){
-     return super.radius; 
     }
     public void removeLife(){
         lives--;
     }
 
-    /* All other methods that add functionality */
     public void move(){
         /*  set the position of the shooter and draws it  */
         // if the shooter touches edge invert velocity
-        if(Math.abs(super.xCoord + super.xVelocity) + super.radius > 640)       super.xVelocity = -super.xVelocity;
+        if(Math.abs(super.xCoord + super.xVelocity) + super.radius > 640){
+            super.xVelocity = -super.xVelocity;
+        }
         // prevents shooting backwards
-        if(angleBarrel <= -90 || angleBarrel >= 90)           radialVelocityBarrel = -radialVelocityBarrel;
+        if(angleBarrel >= 180 || angleBarrel <= 0) {
+            radialVelocityBarrel = -radialVelocityBarrel;
+        }
 
         //  The shooter movement
         super.xCoord= super.xCoord + super.xVelocity;
         // The barrels rotational movement
         angleBarrel = angleBarrel + radialVelocityBarrel;
-        xCoordBarrel = super.radius*Math.cos(angleBarrel*Math.PI/180+90) + xCoord;
-        yCoordBarrel = super.radius*Math.sin(angleBarrel*Math.PI/180+90) + yCoord;
+        xCoordBarrel =  xCoord + super.radius * Math.cos(angleBarrel*Math.PI/180);
+        yCoordBarrel = yCoord + super.radius *Math.sin(angleBarrel*Math.PI/180);
         render();
     }
    
     public void render()
     {
-        if(getLives() == 3){
-            StdDraw.setPenColor(StdDraw.BLUE);
-        }
-        if(getLives() == 2){
-            StdDraw.setPenColor(StdDraw.ORANGE);
-        }
-        if(getLives() == 1){
-            StdDraw.setPenColor(StdDraw.YELLOW);
-        }
-        // Redrawing the Barrel
-        //StdDraw.filledCircle(xCoordBarrel,yCoordBarrel,radiusBarrel);
-        //  Redrawing the Shooter
-        StdDraw.picture(super.xCoord,super.yCoord,"PlayerBlue.png",radius*5,radius*5,angleBarrel);
+        double scaledAngleBarrel = angleBarrel - 90;
+        StdDraw.picture(super.xCoord,super.yCoord,"PlayerBlue.png",super.radius*5,super.radius*5, scaledAngleBarrel);
     }
 
     public void resetState(int startXCoord, int startYCoord) {
         xCoord = startXCoord;
         yCoord = startYCoord;
         radialVelocityBarrel = 0;
-        xCoordBarrel = super.radius*Math.cos(INITIAL_ANGLE_BARREL);
-        yCoordBarrel = super.radius*Math.sin(INITIAL_ANGLE_BARREL);
+        xCoordBarrel = super.xCoord;
+        yCoordBarrel = super.getRadius() + super.yCoord;
     }
 }
