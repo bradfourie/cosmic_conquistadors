a566da06d84604f0ad5e4b6a90d9cfe3d4114fe6
Bradley Fourie <30635808+bradfourie@users.noreply.github.com>
2018-04-27 20:22:27 +0200

    Added JavaDoc comments

--- HeavyEnemy.java
+++ HeavyEnemy.java
@@ -1,11 +1,35 @@
+
+/**
+ * A model object that extends the enemy class that has properties to create
+ * an enemy that is larger, and that has more lives than a normal enemy.
+ *
+ * @author Bradley Fourie
+ * @author Heinrich Benz
+ * @author Daniel Banks
+ * @see Enemy
+ */
+
 public class HeavyEnemy extends Enemy {
 
+    /**
+     * Class constructor that creates a heavy enemy at the specified coordinates, heavy enemies have 
+     * default properties of x-velocities with a magnitude of 5, y-velocities with the magnitude of 50 downwards,
+     * 5 lives and a shootProbability with a range of 5.
+     * 
+     * @param xCoord the x-coordinate of the heavy enemy
+     * @param yCoord the y-coordinate of the heavy enemy
+     * @see Enemy
+     */
     public HeavyEnemy(double xCoord, double yCoord){
-      super(xCoord, yCoord, 5, -50, 20 , 5, 1500);
+        super(xCoord, yCoord, 5, -50, 20 , 5, 1500);
     }
 
+    /**
+     * A method that renders the heavy enemy at a specific coordinate point on the canvas using the picture method
+     * of StdDraw, the width and height of the sprite to be drawn is specified as the diameter of the sprite.
+     */
     public void render(){
-      StdDraw.picture(super.xCoord,super.yCoord,"EnemyYellow.png",super.radius*2,super.radius*2);
+        StdDraw.picture(super.xCoord,super.yCoord,"EnemyYellow.png",super.radius*2,super.radius*2);
     }
 
 }
