f84fc811ba49d37e0a5f7396ddb4916335c1c5e2
Bradley Fourie <30635808+bradfourie@users.noreply.github.com>
2018-04-27 19:44:01 +0200

    Added JavaDoc comments

--- Critter.java
+++ Critter.java
@@ -1,4 +1,14 @@
 
+/**
+ * An interface which has the properties to specify an entity which can move
+ * and interact with the canvas
+ * 
+ * @author Bradley Fourie
+ * @author Heinrich Benz
+ * @author Daniel Banks
+ * @see Critter
+ */
+
 public interface Critter {
 
     public void setXVelocity(double xVelocity);
