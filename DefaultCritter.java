ab65d08445d8a2009cd24c04e813679bdba64ee5
Bradley Fourie <30635808+bradfourie@users.noreply.github.com>
2018-04-27 19:36:16 +0200

    Added JavaDoc comments

--- DefaultCritter.java
+++ DefaultCritter.java
@@ -1,3 +1,17 @@
+/**
+ * DefaultCritter is the base class for all of the entities that will be
+ * created and used in the Cosmic-Conquistadors game, this class implements
+ * the Critter interface which gives it the methods to change the coordinates,
+ * velocities and radius of the entity.
+ *
+ * @author Bradley Fourie
+ * @author Heinrich Benz
+ * @author Daniel Banks
+ * @see Critter
+ * @see Enemy
+ * @see Shooter
+ * @see Missile
+ */
 public class DefaultCritter implements Critter {
 
     protected double xCoord;
@@ -6,6 +20,16 @@ public class DefaultCritter implements Critter {
     protected double yVelocity;
     protected double radius;
 
+    /**
+     * Constructor that creates the entity using the variables needed to specify the position,
+     * movement and radius of the entity
+     *
+     * @param xCoord the x-coordinate the entity is to be initialised at
+     * @param yCoord the y-coordinate the entity is to be initialised at
+     * @param xVelocity the x-velocity of the entity
+     * @param yVelocity the y-velocity of the entity
+     * @param radius the radius or "size" of the entity
+     */
     public DefaultCritter(double xCoord, double yCoord, double xVelocity, double yVelocity, double radius){
         this.xCoord = xCoord;
         this.yCoord = yCoord;
@@ -13,20 +37,25 @@ public class DefaultCritter implements Critter {
         this.yVelocity = yVelocity;
         this.radius = radius;
     }
+
     public void setXVelocity(double xVelocity){
         this.xVelocity = xVelocity;
     }
+
     public void setYVelocity(double yVelocity){
         this.yVelocity = yVelocity;
     }
+
     public double getYCoord(){
         return yCoord;
     }
+
     public double getXCoord(){
         return xCoord;
     }
+    
     public double getRadius(){
         return radius;
     }
-    
+
 }
