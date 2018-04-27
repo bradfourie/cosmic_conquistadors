671de9244c9912c4341c9997d5d564ee0be84b12
Bradley Fourie <30635808+bradfourie@users.noreply.github.com>
2018-04-27 21:11:11 +0200

    Added JavaDoc comments

--- PowerUp.java
+++ PowerUp.java
@@ -1,22 +1,44 @@
+/**
+ * A model object for a powerup with properties for coordinates, radius, velocity and the type of powerup
+ */
 
 public class PowerUp {
+  /**
+   * Constant values that correspond to the type of powerup
+   */
+  private final int NORMAL_MISSILE = -1, GATLING_MISSILE = 0, TRI_MISSILE = 1, SUPER_MISSILE = 2, FAST_MISSILE = 3, EXTRA_LIFE = 4;
 
-  private final int NORMAL_MISSILE =-1, GATLING_MISSILE =0, TRI_MISSILE = 1, SUPER_MISSILE = 2, FAST_MISSILE = 3, EXTRA_LIFE = 4;
-  
-  private double x;
-  private double y;
-  private double length;
-  private double velocity;
+  private double xCoord;
+  private double yCoord;
+  private double radius;
+  private double yVelocity;
   private int power;
-  
-  public PowerUp(int x, int y, double length){
-    this.x = x;
-    this.y = y;
-    this.length = length;
-    this.velocity = 8;
+
+  /**
+   * Class constructor that creates a powerup at the specified coordinates, powerups have default 
+   * y velocities with a magnitude of 8, the constructor generates a random number and assigns a type 
+   * to the powerup using the method choosePowerUp.
+   *
+   * @param xCoord the starting x-coordinate of the powerup
+   * @param yCoord the starting y-coordinate of the powerup
+   * @param radius the radius of the powerup
+   */
+  public PowerUp(int xCoord, int yCoord, double radius){
+    this.xCoord = xCoord;
+    this.yCoord = yCoord;
+    this.radius = radius;
+    this.yVelocity = 8;
     double selection = Math.random();
     choosePowerUp(selection);
   }
+  
+  /**
+   * A method that recieves a variable selection as its input and assigns a type to
+   * the powerup according to the interval in which selection lies.
+   * 
+   * @param selection the randomly generated number which is used to assign a type
+   *                  to the powerup
+   */
   private void choosePowerUp(double selection){
     if(selection < 0.05){
       this.power = GATLING_MISSILE; //5%
@@ -34,40 +56,47 @@ public class PowerUp {
       this.power = EXTRA_LIFE; // 2%
     }
   }
-  
+
   public double getYCoord(){
-    return y;
+    return yCoord;
   }
   public double getXCoord(){
-    return x; 
+    return xCoord;
   }
   public int getPower(){
-    return power; 
+    return power;
   }
-  
   public double getRadius(){
-    return length;
+    return radius;
+  }
+
+  /**
+   * Moves the powerup downwards on the canvas according to its yVelocity, and sets it colour 
+   * according to the type of the powerup.
+   */
+  public void move(){
+    yCoord = yCoord - yVelocity;
+    switch(this.power) {
+      case GATLING_MISSILE:
+        StdDraw.setPenColor(StdDraw.BLUE);
+        break;
+      case TRI_MISSILE:
+        StdDraw.setPenColor(StdDraw.CYAN);
+        break;
+      case SUPER_MISSILE:
+        StdDraw.setPenColor(StdDraw.YELLOW);
+        break;
+      case FAST_MISSILE:
+        StdDraw.setPenColor(StdDraw.MAGENTA);
+        break;
+      case EXTRA_LIFE:
+        StdDraw.setPenColor(StdDraw.GREEN);
+        break;
+    }
+    render();
   }
   
-  public void move(){
-    y = y - velocity;
-    switch(this.power){
-      case GATLING_MISSILE: 
-            StdDraw.setPenColor(StdDraw.BLUE);
-            break;
-      case TRI_MISSILE: 
-            StdDraw.setPenColor(StdDraw.CYAN);
-            break;
-      case SUPER_MISSILE: 
-            StdDraw.setPenColor(StdDraw.YELLOW);
-            break;
-      case FAST_MISSILE: 
-            StdDraw.setPenColor(StdDraw.MAGENTA);
-            break;
-      case EXTRA_LIFE: 
-            StdDraw.setPenColor(StdDraw.GREEN);
-            break;
-    }
-    StdDraw.filledCircle(x , y , length);
+  public void render(){
+    StdDraw.filledCircle(xCoord, yCoord, radius);
   }
 }
