0c6f3794d83866261bc8c2ffbf9c684f5059511e
Heinrich12345 <37140717+Heinrich12345@users.noreply.github.com>
2018-04-29 15:46:10 +0200

    Missile and NormalMissile Javadoc

--- Missile.java
+++ Missile.java
@@ -1,19 +1,42 @@
+/**
+ *  This class extends DefaultCritter and serves as a model object to create
+ *  different types of missiles
+ *
+ *  @author Bradley Fourie
+ *  @author Daniel Banks
+ *  @author Heinrich Benz
+ *  @see DefaultCritter
+ */
 public class Missile extends DefaultCritter{
-    private int numBounced; //counts the number of bounces a missile has made
-    protected double angle; //the angle that determines the direction the bullet is facing
-
+    private int numBounced;
+    protected double angle;
+    
+    /**
+     * Class constructor that creates a missile using the DefaultCritter class as a basis, also adds properties
+     * to missile by specifying the angle at which the bullet was shot.
+     *
+     * @param xCoord the x-coordinate of the missile
+     * @param yCoord the y-coordinate of the missile
+     * @param xVelocity the x-velocity of the missile
+     * @param yVelocity the y-velocity of the missile
+     * @param radius the radius or "size" of the missile
+     * @see DefaultCritter
+     */
     public Missile(double xCoord, double yCoord, double xVelocity, double yVelocity, double radius){
-        //here the magical constant 5 is the magnitude of the velocity
-        //and the magical constant 1.5 is the radius of the missile
         super(xCoord, yCoord, xVelocity, yVelocity, radius);
         this.numBounced = 0;
         this.angle = Math.atan((yVelocity/xVelocity));
     }
-
+    
     public int getNumBounced(){
         return numBounced;
     }
-
+    
+    /**
+     * Indicates that the bullet has bounced and cannot bounce again,
+     * inverts the and and x velocity of the bullet to simulate bouncing
+     *
+     */
     public void wallBounce(){
         numBounced++;
         super.xVelocity = - super.xVelocity;
@@ -32,11 +55,13 @@ public class Missile extends DefaultCritter{
         return super.radius;
     }
 
-
+    /**
+     *  Changes the pen colour to yellow,
+     *  moves the missile in both the X direction and Y direction
+     */
     public void move(){
         StdDraw.setPenColor(StdDraw.YELLOW);
         super.xCoord = super.xCoord + super.xVelocity;
         super.yCoord = super.yCoord + super.yVelocity;
     }
-
 }
--- NormalMissile.java
+++ NormalMissile.java
@@ -1,16 +1,37 @@
+/**
+ *  This class extends Missile
+ *
+ *  @author Bradley Fourie
+ *  @author Daniel Banks
+ *  @author Heinrich Benz
+ *  @see Missile
+ */
 public class NormalMissile extends Missile{
-
+  
+  /**
+     * Class constructor that creates a NormalMissile using the Missile class as a basis,
+     * uses the shooter it is shot from to determine its properties.
+     *
+     * @param shooter the shooter that shoots the NormalMissile
+     * @see Missile
+     */
   public NormalMissile(Shooter shooter){
     super(shooter.getXCoordBarrel(), shooter.getYCoordBarrel(), 15 * Math.cos(shooter.getAngleBarrel()*Math.PI/180), 15 * Math.sin(shooter.getAngleBarrel()*Math.PI/180), 3);
   }
-
+  
+  /**
+     * Class constructor that creates a NormalMissile using the Missile class as a basis,
+     * uses the shooter it is shot from as well as an offset angle to determine its properties.
+     *
+     * @param shooter the shooter that shoots the NormalMissile
+     * @param angleOffset an offset angle for when a bullet shoots at a different angle to the shooter
+     * @see Missile
+     */
   public NormalMissile(Shooter shooter, double angleOffset){
     super(shooter.getXCoordBarrel(), shooter.getYCoordBarrel(), 15 * Math.cos((shooter.getAngleBarrel() + angleOffset)*Math.PI/180), 15 * Math.sin((shooter.getAngleBarrel() + angleOffset)*Math.PI/180), 3);
   }
   
   public void render(){
-    StdDraw.picture(super.xCoord,super.yCoord,"BulletYellow.png",super.radius*5,super.radius*2, super.angle*Math.PI/180);
+    StdDraw.picture(super.xCoord,super.yCoord,"BulletYellow.png",super.radius*5,super.radius*2, super.angle);
   }
-
 }
-
