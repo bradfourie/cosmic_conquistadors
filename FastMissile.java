54201a78c2998de0a914215200934d9a3bceb711
Heinrich12345 <37140717+Heinrich12345@users.noreply.github.com>
2018-04-30 04:37:59 +0200

    SuperMissile and FastMissile Javadocs

--- FastMissile.java
+++ FastMissile.java
@@ -1,9 +1,26 @@
+/**
+ *  This class extends Missile
+ *
+ *  @author Bradley Fourie
+ *  @author Daniel Banks
+ *  @author Heinrich Benz
+ *  @see Missile
+ */
 public class FastMissile extends Missile{
 
+  /**
+     * Class constructor that creates a FastMissile using the Missile class as a basis,
+     * uses the shooter it is shot from to determine its properties.
+     *
+     * @param shooter the shooter that shoots the NormalMissile
+     * @see Missile
+     */
  public FastMissile(Shooter shooter){
     super(shooter.getXCoordBarrel(), shooter.getYCoordBarrel(), 25 * Math.cos((shooter.getAngleBarrel())*Math.PI/180), 25 * Math.sin((shooter.getAngleBarrel())*Math.PI/180), 3);
  }
-  
+  /**
+    * Renders the FastMissile on the canvas.
+    */
  public void render(){
   StdDraw.picture(super.xCoord,super.yCoord,"BulletGreen.png",super.radius*5,super.radius*2, super.angle*Math.PI/180);
  }
--- SuperMissile.java
+++ SuperMissile.java
@@ -1,8 +1,28 @@
+/**
+ *  This class extends Missile
+ *
+ *  @author Bradley Fourie
+ *  @author Daniel Banks
+ *  @author Heinrich Benz
+ *  @see Missile
+ */
 public class SuperMissile extends Missile{
-  	public SuperMissile(Shooter shooter){
-    	super(shooter.getXCoordBarrel(), shooter.getYCoordBarrel(), 15*Math.cos(shooter.getAngleBarrel()*Math.PI/180), 15*Math.sin(shooter.getAngleBarrel()*Math.PI/180), 7);
-  	}
-  	public void render(){
-    	StdDraw.picture(super.xCoord,super.yCoord,"BulletYellow.png",super.radius*5,super.radius*2, super.angle*Math.PI/180);
-  	}
+  
+  /**
+     * Class constructor that creates a SuperMissile using the Missile class as a basis,
+     * uses the shooter it is shot from to determine its properties.
+     *
+     * @param shooter the shooter that shoots the SuperMissile
+     * @see Missile
+     */
+   public SuperMissile(Shooter shooter){
+     super(shooter.getXCoordBarrel(), shooter.getYCoordBarrel(), 15*Math.cos(shooter.getAngleBarrel()*Math.PI/180), 15*Math.sin(shooter.getAngleBarrel()*Math.PI/180), 7);
+   }
+   
+   /**
+    * Renders the SuperMissile on the canvas.
+    */
+   public void render(){
+     StdDraw.picture(super.xCoord,super.yCoord,"BulletYellow.png",super.radius*5,super.radius*2, super.angle*Math.PI/180);
+   }
 }
